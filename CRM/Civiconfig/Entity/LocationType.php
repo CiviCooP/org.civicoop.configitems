912b1270cd89647d02f4f9ba4c52cf9442e24f88
Johan Vervloet <johanv@johanv.org>
2016-06-06 16:35:13 +0200

    Support for location types.
    This builds further on PR #3, which keeps the changes in Config.php
    minimal.

---
+++ CRM/Civiconfig/Entity/LocationType.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Class for RelationshipType configuration
+ *
+ * @author Erik Hommel (CiviCooP) <erik.hommel@civicoop.org>
+ * @date 3 Feb 2016
+ * @license AGPL-3.0
+ */
+class CRM_Civiconfig_Entity_LocationType extends CRM_Civiconfig_Entity {
+
+  protected $_apiParams = array();
+
+  /**
+   * CRM_Civiconfig_RelationshipType constructor.
+   */
+  public function __construct() {
+    $this->_apiParams = array();
+  }
+
+  /**
+   * Method to validate params passed to create
+   *
+   * @param $params
+   * @throws Exception when required param not found
+   */
+  private function validateCreateParams($params) {
+    if (!isset($params['name']) || empty($params['name'])) {
+      throw new Exception('When trying to create a Location Type, name is mandatory
+        parameter and can not be empty in class ' . get_class());
+    }
+    // The line below is in a strange place in the code. But I'll keep it
+    // there, because it is there as well for every other entity type.
+    $this->_apiParams = $params;
+  }
+
+  /**
+   * Method to create or update a location type
+   *
+   * @param array $params
+   * @return mixed
+   * @throws Exception when error from API LocationType Create
+   */
+  public function create(array $params) {
+    $this->validateCreateParams($params);
+    $existing = $this->getWithName($this->_apiParams['name']);
+    if (isset($existing['id'])) {
+      $this->_apiParams['id'] = $existing['id'];
+    }
+    try {
+      civicrm_api3('LocationType', 'Create', $this->_apiParams);
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception('Could not create or update location type with name '.$this->_apiParams['name']
+        .', error from API LocationType Create: '.$ex->getMessage());
+    }
+  }
+
+  /**
+   * Function to get the location type with a name
+   *
+   * @param string $name
+   * @return array|bool
+   * @access public
+   * @static
+   */
+  public function getWithName($name) {
+    try {
+      return civicrm_api3('LocationType', 'Getsingle',
+        array('name' => $name));
+    } catch (CiviCRM_API3_Exception $ex) {
+      return FALSE;
+    }
+  }
+}
